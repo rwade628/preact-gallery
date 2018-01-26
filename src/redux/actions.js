11f4cc75d8c5a839a5820147a0ba3e067203270a
Ryan Wade <ryanwade628@gmail.com>
2018-01-26 07:30:35 -0500

    Implemting example api calls

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,14 +1,45 @@
-export function getGallery(selectedGallery) {
-	return {
-		type: 'GET_GALLERY',
-		selectedGallery
-	};
+export function getGallery(selectedLocation, selectedGallery) {
+	return (dispatch, getState) => {
+	    return new Promise((resolve, reject) => {
+			var url = `/api/gallery/${selectedLocation}/${selectedGallery}`
+			console.log('url', url)
+			fetch(url).then((response) => {
+				return response.json()
+			}).then((json) => {
+				console.log('dispatching', json)
+				dispatch({
+					type: 'GET_GALLERY',
+					selectedGallery: json
+				})
+			resolve()
+			}).catch((ex) => {
+				console.log('parsing failed test', ex)
+				reject()
+			})
+	    })
+	}
 }
 
-export function getAlbums() {
-	return {
-		type: 'GET_ALBUMS'
-	};
+
+export function getAlbums(album) {
+	return (dispatch, getState) => {
+	    return new Promise((resolve, reject) => {
+			const url = `/api/list/${album}`
+			fetch(url).then((response) => {
+				return response.json()
+			}).then((json) => {
+				console.log('dispatching', json)
+				dispatch({
+				  type    : 'GET_ALBUMS',
+				  albumList : json
+				})
+			resolve()
+			}).catch((ex) => {
+				console.log('parsing failed test', ex)
+				reject()
+			})
+	    })
+	}
 }
 
 export function setSelected(selectedItem) {
